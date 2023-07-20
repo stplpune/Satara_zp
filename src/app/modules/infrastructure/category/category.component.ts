84bb49e7c95b62b4408e1ca52cb13a500800ade0
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-07-20 19:00:25 +0530

    category page design

--- src/app/modules/infrastructure/category/category.component.ts
+++ src/app/modules/infrastructure/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AddCategoryComponent } from './add-category/add-category.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-category',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent {
+  viewStatus='Table';
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AddCategoryComponent,{
+      width: '400px',
+      disableClose: true,
+      autoFocus: false
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: any;
+  weight: any;
+  symbol: any;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
--- src/app/modules/infrastructure/category/category.module.ts
+++ src/app/modules/infrastructure/category/category.module.ts
@@ -1,17 +1,56 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryComponent } from './category.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TranslateModule } from '@ngx-translate/core';
+import { AutofocusDirective } from 'src/app/core/directives/autofocus.directive';
+import { GlobalDialogComponent } from 'src/app/shared/components/global-dialog/global-dialog.component';
+import { GridViewComponent } from 'src/app/shared/components/grid-view/grid-view.component';
+import { PageStatisticsComponent } from 'src/app/shared/components/page-statistics/page-statistics.component';
+import { TableComponent } from 'src/app/shared/components/table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { AddCategoryComponent } from './add-category/add-category.component';
 
 
 @NgModule({
   declarations: [
-    CategoryComponent
+    CategoryComponent,
+    AddCategoryComponent
   ],
   imports: [
     CommonModule,
-    CategoryRoutingModule
+    CategoryRoutingModule,
+    MatDialogModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatMenuModule,
+    ReactiveFormsModule,
+    TableComponent,
+    PageStatisticsComponent,
+    GlobalDialogComponent,
+    TranslateModule,
+    AutofocusDirective,
+    GridViewComponent,
+    MatTooltipModule,
+    MatTableModule
   ]
 })
 export class CategoryModule { }

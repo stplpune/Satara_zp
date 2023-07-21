36b2f08fc0f62574a72ef8048b209a25cc18e017
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-07-21 15:34:31 +0530

    asset page design

---
+++ src/app/modules/infrastructure/asset/add-asset/add-asset.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-asset',
+  templateUrl: './add-asset.component.html',
+  styleUrls: ['./add-asset.component.scss']
+})
+export class AddAssetComponent {
+  editData:any;
+  editObj:any;
+}

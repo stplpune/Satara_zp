bd77563672b7f92ac9167b313f5c22221a098dcd
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-07-21 11:58:21 +0530

    add-updated-student-registration page-add input field and button

--- src/app/modules/masters/student-registration/add-update-student-registration/add-update-student-registration.component.ts
+++ src/app/modules/masters/student-registration/add-update-student-registration/add-update-student-registration.component.ts
@@ -17,7 +17,7 @@ import { WebStorageService } from 'src/app/core/services/web-storage.service';
   styleUrls: ['./add-update-student-registration.component.scss']
 })
 export class AddUpdateStudentRegistrationComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','Action'];
   dataSource = ELEMENT_DATA;
   
   stuRegistrationForm!: FormGroup
@@ -489,17 +489,11 @@ export interface PeriodicElement {
   position: number;
   weight: number;
   symbol: string;
+  Action:any;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H',Action:''},
+  {position: 2, name: 'Hydrogen', weight: 1.0079, symbol: 'H',Action:''},
+  {position: 3, name: 'Hydrogen', weight: 1.0079, symbol: 'H',Action:''},
 ];

e09e070f65117aae222e8668af7f22503199c1bd
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-07-27 18:28:00 +0530

    outward-item page design

--- src/app/modules/infrastructure/outward-item/outward-item.component.ts
+++ src/app/modules/infrastructure/outward-item/outward-item.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { AddOutwardItemComponent } from './add-outward-item/add-outward-item.component';
 
 @Component({
   selector: 'app-item-transfer',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./outward-item.component.scss']
 })
 export class OutwardItemComponent {
-
-}
+  viewStatus="Table";
+  cardViewFlag: boolean = false;
+  filterForm!: FormGroup;
+  constructor (private fb: FormBuilder,
+    public dialog: MatDialog){}
+    filterFormData() {
+      this.filterForm = this.fb.group({
+        CategoryId: [''],
+        SubCategoryId: [''],
+        villageId:[''],
+        ItemsId: [''],
+        textSearch: ['']
+      })
+    }
+    openDialog() {
+      const dialogRef = this.dialog.open(AddOutwardItemComponent,
+        {
+          width: '500px',
+          disableClose: true,
+          autoFocus: false
+        });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
+    }
+  }
+  

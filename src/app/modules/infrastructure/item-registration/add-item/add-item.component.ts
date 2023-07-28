b843e6cffab5b0be24cda6350ddc22b653a257f3
Pradip-ShauryaTechnosoft <pradip.ganjale@shauryatechnosoft.com>
2023-07-28 18:41:59 +0530

    add and update functionality complete in item registration form

--- src/app/modules/infrastructure/item-registration/add-item/add-item.component.ts
+++ src/app/modules/infrastructure/item-registration/add-item/add-item.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ApiService } from 'src/app/core/services/api.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { ErrorsService } from 'src/app/core/services/errors.service';
+import { MasterService } from 'src/app/core/services/master.service';
+import { ValidationService } from 'src/app/core/services/validation.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
   selector: 'app-add-item',
@@ -10,5 +18,115 @@ export class AddItemComponent {
   itemForm!: FormGroup;
   editFlag: boolean = false;
   editObj: any;
+  languageFlag!:string;
+  categoryArr = new Array();
+  subCategoryArr = new Array();
 
+  constructor (private fb: FormBuilder, 
+    private apiService: ApiService,
+    public webService: WebStorageService,    
+    private errors: ErrorsService,
+    private masterService: MasterService,
+    private commonMethods: CommonMethodsService,
+    public dialogRef: MatDialogRef<AddItemComponent>,
+    private ngxSpinner: NgxSpinnerService,
+    public validators: ValidationService,
+    @Inject(MAT_DIALOG_DATA) public data: any){}
+
+ 
+
+  ngOnInit(): void {   
+    this.languageFlag = this.webService.languageFlag;
+    this.formData();
+    this.getAllCategory();
+  }
+
+  get f() {
+    return this.itemForm.controls;
+  }
+
+  formData(){
+    this.itemForm = this.fb.group({
+        "id": [this.data?.id ||0],
+        "categoryId": ['',[Validators.required]],
+        "category": [this.data?.category ||''],
+        "m_Category": [this.data?.m_Category ||''],
+        "subCategoryId": ['',[Validators.required]],
+        "subCategory": [this.data?.subCategory ||''],
+        "m_SubCategory": [this.data?.m_SubCategory ||''],
+        "itemName": [this.data?.itemName ||'',[Validators.required,Validators.pattern(this.validators.name)]],
+        "m_ItemName": [this.data?.m_ItemName ||''],
+        "description": [this.data?.description ||'',[Validators.required,Validators.pattern(this.validators.name)]],
+        "createdBy": [0],
+        "isDeleted": true,
+        "lan": [this.data?.lan ||'EN']
+      
+    })
+
+  }
+
+  getAllCategory() {
+    this.categoryArr = [];
+    this.masterService.GetAllAssetCategory(this.languageFlag).subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200) {
+          this.categoryArr= res.responseData;
+          this.data ? (this.f['categoryId'].setValue(this.data.categoryId),this.getAllSubCategory()):'';
+        } else {
+          this.commonMethods.checkEmptyData(res.statusMessage) == false ? this.errors.handelError(res.statusCode) : this.commonMethods.showPopup(res.statusMessage, 1);
+          this.categoryArr = [];
+        }
+      },
+      error: ((err: any) => { this.errors.handelError(err.statusCode) })
+    });
+  }
+
+  getAllSubCategory() {
+    let catId = this.itemForm.value.categoryId; 
+    this.subCategoryArr = [];
+    this.masterService.GetAssetSubCateByCateId(catId,this.languageFlag).subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200) {
+          this.subCategoryArr= res.responseData;
+          this.data ? this.f['subCategoryId'].setValue(this.data.subCategoryId):'';
+        } else {
+          this.commonMethods.checkEmptyData(res.statusMessage) == false ? this.errors.handelError(res.statusCode) : this.commonMethods.showPopup(res.statusMessage, 1);
+          this.subCategoryArr = [];
+        }
+      },
+      error: ((err: any) => { this.errors.handelError(err.statusCode) })
+    });
+  }
+
+  onSubmit(){
+    let postData = this.itemForm.value;
+    if(this.itemForm.invalid){
+      return;
+    }else{
+      this.ngxSpinner.show();
+      let url = this.data ? 'UpdateItem' : 'AddItemMaster'
+      this.apiService.setHttp('post', 'zp-satara/ItemMaster/' + url, false, postData, false, 'baseUrl');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => {
+          if (res.statusCode == 200) {
+            this.ngxSpinner.hide();
+            this.commonMethods.showPopup(res.statusMessage, 0);
+            this.dialogRef.close('yes')
+     
+          } else {
+            this.ngxSpinner.hide();
+            this.commonMethods.checkEmptyData(res.statusMessage) == false ? this.errors.handelError(res.statusCode) : this.commonMethods.showPopup(res.statusMessage, 1);
+          }
+        },
+        error: ((err: any) => {  this.errors.handelError(err.statusCode) })
+      });
+    }   
+  }
+
+  clearDropdown(dropdown: string) {   
+    if (dropdown == 'CategoryId') {
+      this.itemForm.controls['subCategoryId'].setValue(0);     
+      this.subCategoryArr = [];
+    }
+  }
 }
--- src/app/modules/infrastructure/item-registration/item-registration.component.ts
+++ src/app/modules/infrastructure/item-registration/item-registration.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AddItemComponent } from './add-item/add-item.component';
+// import { NgxSpinnerService } from 'ngx-spinner';
+import { ApiService } from 'src/app/core/services/api.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { ErrorsService } from 'src/app/core/services/errors.service';
+import { MasterService } from 'src/app/core/services/master.service';
+import { DownloadPdfExcelService } from 'src/app/core/services/download-pdf-excel.service';
+import { DatePipe } from '@angular/common';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { GlobalDialogComponent } from 'src/app/shared/components/global-dialog/global-dialog.component';
 
 @Component({
   selector: 'app-item-registration',
@@ -13,29 +23,259 @@ export class ItemRegistrationComponent {
   viewStatus="Table";
   cardViewFlag: boolean = false;
   filterForm!: FormGroup;
+  pageNumber: number = 1;
+  searchContent = new FormControl('');
+  totalCount: number = 0;
+  tableDataArray = new Array();
+  tableDatasize!:number;
+  languageFlag!:string;
+  highLightFlag : boolean = true;
+  isWriteRight!: boolean;
+  categoryArr = new Array();
+  subCategoryArr = new Array();
+  itemArr = new Array();
+  resultDownloadArr = new Array();
+  deleteObj:any;
+
+
+  displayedColumns = [ 'srNo','category', 'subCategory', 'itemName', 'description','action'];
+  marathiDisplayedColumns = ['srNo','m_Category', 'm_SubCategory','m_ItemName', 'description', 'action'];
+  displayedheaders = ['Sr. No.',' Category', 'Sub Category', 'Item Name', 'Description','action'];
+  marathiDisplayedheaders = ['अनुक्रमांक','श्रेणी', 'उप श्रेणी', 'वस्तूचे नाव', 'वर्णन', 'कृती'];
+
+
   constructor (private fb: FormBuilder,
-    public dialog: MatDialog){}
+    public dialog: MatDialog,
+    private apiService: ApiService,
+    public webService: WebStorageService,
+    private commonMethodS: CommonMethodsService,
+    private errors: ErrorsService,
+    private masterService: MasterService,
+    private commonMethods: CommonMethodsService,
+    private downloadFileService: DownloadPdfExcelService,
+    public datepipe: DatePipe,
+    private ngxSpinner: NgxSpinnerService){}
+
+ 
+
+  ngOnInit(): void {
+    this.languageFlag = this.webService.languageFlag;
+    this.getTableData();
+    this.filterFormData();
+    this.getAllCategory();
+  }
 
   filterFormData() {
     this.filterForm = this.fb.group({
       CategoryId: [''],
       SubCategoryId: [''],
       villageId:[''],
-      ItemsId: [''],
+      // ItemsId: [''],
       textSearch: ['']
     })
   }
-  openDialog() {
-    const dialogRef = this.dialog.open(AddItemComponent,
-      {
-        width: '500px',
-        disableClose: true,
-        autoFocus: false
-      });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+  getTableData(flag?: string) {
+    let formValue =this.filterForm?.value
+    this.ngxSpinner.show();
+    this.pageNumber = flag == 'filter' ? 1 : this.pageNumber;
+    let pageNo = this.cardViewFlag ? (this.pageNumber) : this.pageNumber;    
+    let str=`CategoryId=${formValue?.CategoryId || 0}&SubCategoryId=${formValue?.SubCategoryId || 0}&pageno=${pageNo}&pagesize=10&TextSearch=${formValue?.textSearch || ''}&lan=${this.languageFlag}`   
+    let reportStr = `CategoryId=${formValue?.CategoryId || 0}&SubCategoryId=${formValue?.SubCategoryId || 0}&pageno=${pageNo}&pagesize=${this.tableDatasize *10}&TextSearch=${formValue?.textSearch || ''}&lan=${this.languageFlag}`  
+    this.apiService.setHttp('GET', 'zp-satara/ItemMaster/GetAllItem?' + (flag == 'pdfFlag' ? reportStr : str), false, false, false, 'baseUrl');     
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {   
+        if (res.statusCode == 200) {
+          this.ngxSpinner.hide();
+          flag != 'pdfFlag' ? this.tableDataArray = res.responseData.responseData1 : this.tableDataArray = this.tableDataArray;
+        // this.tableDataArray = res.responseData.responseData1 
+         this.tableDatasize = res.responseData.responseData2.pageCount 
+         this.resultDownloadArr=[];
+         let data: [] = flag == 'pdfFlag' ? res.responseData.responseData1 : [];
+         flag == 'pdfFlag' ? this.downloadPdf(data) : '';     
+        }else{
+          this.ngxSpinner.hide();
+          this.tableDataArray =[];
+          this.tableDatasize = 0
+        }
+        this.setTableData();
+      },
+      error: ((err: any) => { (this.ngxSpinner.hide(),this.commonMethodS.checkEmptyData(err.statusText) == false) ? this.errors.handelError(err.statusCode) : this.commonMethodS.showPopup(err.statusText, 1); })
+   
     });
   }
+
+  setTableData(){
+    this.highLightFlag =true;
+    let displayedColumns  = [ 'srNo','category', 'subCategory', 'itemName', 'description','action'];
+    let marathiDisplayedColumns = ['srNo','m_Category', 'm_SubCategory','m_ItemName', 'description', 'action'];
+    let tableData = {
+      highlightedrow:true,
+      pageNumber: this.pageNumber,
+      img: 'docPath', blink: '', badge: '', isBlock: '', pagintion: this.tableDatasize > 10 ? true : false,
+      displayedColumns: this.isWriteRight == true ? this.languageFlag == 'EN' ? this.displayedColumns : this.marathiDisplayedColumns : this.languageFlag == 'EN' ? displayedColumns : marathiDisplayedColumns,
+      tableData: this.tableDataArray,
+      tableSize: this.tableDatasize,
+      tableHeaders: this.languageFlag == 'EN' ? this.displayedheaders : this.marathiDisplayedheaders
+    };
+    this.highLightFlag?tableData.highlightedrow=true:tableData.highlightedrow=false,
+    this.apiService.tableData.next(tableData);
+  }
+
+  downloadPdf(data: any) { 
+    data.find((ele: any, i: any) => {
+      let obj = {
+        srNo: i + 1,
+        category: ele.category,
+        subCategory: ele.subCategory,
+        itemName: ele.itemName,
+        description: ele.description,
+      }
+      this.resultDownloadArr.push(obj);
+    });
+    // download pdf call
+    if (this.resultDownloadArr.length > 0) {
+      let keyPDFHeader = ["Sr.No.", "Category", "Sub Category", "Item Name", "Description"];
+      let ValueData =
+        this.resultDownloadArr.reduce(
+          (acc: any, obj: any) => [...acc, Object.values(obj).map((value) => value)], []
+        );
+      let objData: any = {
+        'topHedingName': 'Item List',
+        'createdDate': 'Created on:' + this.datepipe.transform(new Date(), 'yyyy-MM-dd, h:mm a')
+      }
+      ValueData.length > 0 ? this.downloadFileService.downLoadPdf(keyPDFHeader, ValueData, objData) : ''
+    }
+  }
+
+
+  
+  childTableCompInfo(obj: any) {
+    switch (obj.label) {
+      case 'Pagination':
+        this.pageNumber = obj.pageNumber;
+        this.getTableData();
+        break;
+      case 'Edit':
+         this.addUpdateItem(obj);
+        break;
+      case 'Delete':
+        this.globalDialogOpen(obj);
+        break;
+      case 'View':
+        // this.openDetailsDialog(obj);
+        break;
+    }
+  }
+
+  getAllCategory() {
+    this.categoryArr = [];
+    this.masterService.GetAllAssetCategory(this.languageFlag).subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200) {
+          this.categoryArr= res.responseData;
+        } else {
+          this.commonMethods.checkEmptyData(res.statusMessage) == false ? this.errors.handelError(res.statusCode) : this.commonMethods.showPopup(res.statusMessage, 1);
+          this.categoryArr = [];
+        }
+      },
+      error: ((err: any) => { this.errors.handelError(err.statusCode) })
+    });
+  }
+
+  getAllSubCategory() {
+    let catId = this.filterForm.value.CategoryId; 
+    this.subCategoryArr = [];
+    this.masterService.GetAssetSubCateByCateId(catId,this.languageFlag).subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200) {
+          this.subCategoryArr= res.responseData;
+        } else {
+          this.commonMethods.checkEmptyData(res.statusMessage) == false ? this.errors.handelError(res.statusCode) : this.commonMethods.showPopup(res.statusMessage, 1);
+          this.subCategoryArr = [];
+        }
+      },
+      error: ((err: any) => { this.errors.handelError(err.statusCode) })
+    });
+  }
+
+  addUpdateItem(obj?:any){
+    const dialogRef = this.dialog.open(AddItemComponent, {
+      width: '500px',
+      disableClose: true,
+      autoFocus: false,
+      data: obj
+ 
+    });
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result == 'yes' && obj) {
+        this.pageNumber = obj.pageNumber;
+        this.getTableData();
+      } else if (result == 'yes') {
+        this.pageNumber = 1;
+        this.getTableData();
+      }
+      this.highLightFlag =false;
+      this.setTableData();
+    });
+  }
+
+  globalDialogOpen(obj: any) {
+    this.deleteObj = obj;
+    let dialoObj = {
+      header: 'Delete',
+      title: this.webService.languageFlag == 'EN' ? 'Do you want to delete School record?' : 'तुम्हाला शाळेचा रेकॉर्ड हटवायचा आहे का?',
+      cancelButton: this.webService.languageFlag == 'EN' ? 'Cancel' : 'रद्द करा',
+      okButton: this.webService.languageFlag == 'EN' ? 'Ok' : 'ओके'
+    }
+    const deleteDialogRef = this.dialog.open(GlobalDialogComponent, {
+      width: '320px',
+      data: dialoObj,
+      disableClose: true,
+      autoFocus: false
+    })
+    deleteDialogRef.afterClosed().subscribe((result: any) => {
+      if (result == 'yes') {
+        this.deteleDialogOpen();
+      }
+      this.highLightFlag=false;
+      
+    })
+  }
+
+  deteleDialogOpen(){
+    let deleteObj = {
+        "id": this.deleteObj.id,
+        "modifiedBy": 0,
+        "modifiedDate": new Date(),
+        "lan": "EN"
+    }
+    this.apiService.setHttp('DELETE', 'zp-satara/ItemMaster/DeleteItem', false, deleteObj, false, 'baseUrl');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200) {
+          this.getTableData();
+          this.commonMethods.showPopup(res.statusMessage, 0);
+        } else {     
+          this.commonMethods.checkEmptyData(res.statusMessage) == false ? this.errors.handelError(res.statusCode) : this.commonMethods.showPopup(res.statusMessage, 1);
+        }
+      },
+      error: ((err: any) => {  this.errors.handelError(err.statusCode) })
+    });
+  }
+
+  clearForm(){
+    this.filterForm.reset();
+    this.getTableData();
+  }
+
+  clearDropdown(dropdown: string) {   
+    if (dropdown == 'CategoryId') {
+      this.filterForm.controls['SubCategoryId'].setValue(0);
+      this.filterForm.controls['textSearch'].setValue('');
+      this.subCategoryArr = [];
+    }
+  }
+
 }
 

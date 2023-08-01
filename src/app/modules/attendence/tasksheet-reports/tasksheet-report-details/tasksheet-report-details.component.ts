1df01e2e9e0de40bd9125f8ac7a9f5b3607f3478
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-08-01 14:52:10 +0530

    create component

---
+++ src/app/modules/attendence/tasksheet-reports/tasksheet-report-details/tasksheet-report-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tasksheet-report-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tasksheet-report-details.component.html',
+  styleUrls: ['./tasksheet-report-details.component.scss']
+})
+export class TasksheetReportDetailsComponent {
+
+}

6b79015a09fff87bb7a16513793b9fc8c45e6b05
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-08-01 13:14:57 +0530

    tasksheet-report page design

--- src/app/modules/attendence/tasksheet-reports/tasksheet-reports.component.ts
+++ src/app/modules/attendence/tasksheet-reports/tasksheet-reports.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasksheet-reports.component.scss']
 })
 export class TasksheetReportsComponent {
-
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','Absent','Action'];
+  dataSource = ELEMENT_DATA;
+  viewStatus='Table';
 }
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  Absent:any;
+  Action:any;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: '0',Absent:'0',Action:''},
+  {position: 2, name: 'Hydrogen', weight: 1.0079, symbol: '2',Absent:'5',Action:''},
+  {position: 3, name: 'Hydrogen', weight: 1.0079, symbol: '1',Absent:'3',Action:''},
+  {position: 4, name: 'Hydrogen', weight: 1.0079, symbol: '5',Absent:'4',Action:''},
+  
+];

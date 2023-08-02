5358842f7c4adb03e162e133ed4efa851b1578e3
dhaneshwareegughane <gughanedhaneshwaree@gmail.com>
2023-08-02 12:04:43 +0530

    add validation

--- src/app/modules/attendence/tasksheet/add-tasksheet/add-tasksheet.component.ts
+++ src/app/modules/attendence/tasksheet/add-tasksheet/add-tasksheet.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ApiService } from 'src/app/core/services/api.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { ErrorsService } from 'src/app/core/services/errors.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
   selector: 'app-add-tasksheet',
@@ -8,5 +13,64 @@ import { FormGroup } from '@angular/forms';
 })
 export class AddTasksheetComponent {
   editFlag:any;
-  eventForm !: FormGroup;
+  attendenceForm !: FormGroup;
+ 
+  constructor(private fb:FormBuilder,
+    private webStorage:WebStorageService,
+    private apiService:ApiService,
+    private commonMethod:CommonMethodsService,
+    private dialogRef: MatDialogRef<AddTasksheetComponent>,
+    private errors:ErrorsService,
+    ){}
+
+  ngOnInit(){
+    this.defaultForm();
+  }
+
+  defaultForm(){
+    this.attendenceForm=this.fb.group({
+      remark:['',[Validators.required]]
+    })
+  }
+
+
+  onSubmit() {
+    console.log(this.attendenceForm.value);
+    
+    if (this.attendenceForm.invalid) {
+      return;
+    }else{
+      let data = this.webStorage.createdByProps();
+    let formData = this.attendenceForm.value;
+    let obj = {
+      "createdBy": data.createdBy,
+      "modifiedBy": data.modifiedBy,
+      "createdDate": data.createdDate,
+      "modifiedDate": data.modifiedDate,
+      "isDeleted": data.isDeleted,
+      "id":  this.editFlag,
+      "category": formData.category,
+      "m_Category":formData.M_category,
+      // "lan": this.languageFlag
+    }
+
+    // let method = this.editFlag ? 'PUT' : 'POST';
+    // let url = this.editFlag ? 'UpdateCategory' : 'AddCategory';
+    this.apiService.setHttp('POST', 'zp-satara/AssetCategory/AddCategory', false, obj, false, 'baseUrl');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == "200") {
+          this.commonMethod.showPopup(res.statusMessage, 0);
+          this.dialogRef.close('yes');
+        } else {
+          this.commonMethod.showPopup(res.statusMessage, 1);
+        }
+
+      },
+      error: ((err: any) => { this.errors.handelError(err) })
+    });
+    } 
+    
+  }
+
 }

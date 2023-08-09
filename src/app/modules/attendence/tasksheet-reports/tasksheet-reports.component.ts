3103a7f86dbd7326ec53cb59cae233b79da476f0
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-08-09 11:31:01 +0530

    tasksheet-report page design

--- src/app/modules/attendence/tasksheet-reports/tasksheet-reports.component.ts
+++ src/app/modules/attendence/tasksheet-reports/tasksheet-reports.component.ts
@@ -19,4 +19,15 @@ export class TasksheetReportsComponent {
   navigateToReport(){
     this.router.navigate(['/teacher-tasksheet']);
   }
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+  
+}
+interface Food {
+  value: string;
+  viewValue: string;
 }

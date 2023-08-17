03e53277fef61aae1ca5a69b3dfe5ea2eeb39d43
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-08-17 12:51:10 +0530

    create share-event-for-android module

---
+++ src/app/modules/share-event-for-android/share-event-for-android.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-share-event-for-android',
+  templateUrl: './share-event-for-android.component.html',
+  styleUrls: ['./share-event-for-android.component.scss']
+})
+export class ShareEventForAndroidComponent {
+
+}

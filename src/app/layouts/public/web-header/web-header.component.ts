047ace3c7f24e58aff12e2bd98892e9a4cebd896
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-08-25 16:52:41 +0530

    web-header page and added sticky scroll to the ts file

--- src/app/layouts/public/web-header/web-header.component.ts
+++ src/app/layouts/public/web-header/web-header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-web-header',
   templateUrl: './web-header.component.html',
   styleUrls: ['./web-header.component.scss']
 })
 export class WebHeaderComponent {
+  @HostListener('window:scroll', ['$event'])
+
+  ngOnInit(){
+    window.addEventListener('scroll', this.scroll, true)
+  }
+  scroll = (): void => {
+     if(window.scrollY >= 500){ // scroll height gretaer than  
+       document.body.style.setProperty('--navbar-scroll', "#276bc6");
+     }else if(window.scrollY < 500){
+       document.body.style.setProperty('--navbar-scroll', "transparent");
+     }
+   }
 
 }

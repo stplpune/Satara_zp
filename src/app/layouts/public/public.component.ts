4d4f4a00c1af733aeb73c856074dde3fe966ef8d
deepalighume <deepali.ghume@shauryatechnosoft.com>
2023-08-25 17:01:31 +0530

    clr clnsole

--- src/app/layouts/public/public.component.ts
+++ src/app/layouts/public/public.component.ts
@@ -17,7 +17,6 @@ export class PublicComponent {
 
   checkScroll() {
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    console.log('[scroll]', scrollPosition);
     if (scrollPosition >= this.topPosToStartShowing) {
       this.isShow = true;
     } else {

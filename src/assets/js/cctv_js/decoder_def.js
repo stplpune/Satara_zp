db1287432a138d03a7dc00b1c626e09147a3987b
deepalighume <deepali.ghume@shauryatechnosoft.com>
2023-09-05 15:39:05 +0530

    added js file for cctv

---
+++ src/assets/js/cctv_js/decoder_def.js
@@ -0,0 +1,8 @@
+const JA_InitDecoderReq = 0;
+const JA_DecoderBufferReq = 1;
+
+const JA_WasmLoadedRsp = 0;
+const JA_DecoderBufferRsp = 1;
+const JA_InitDecoderRsp = 2;
+const JA_FPSRsp = 3;
+const JA_DecodercompleteRsp = 4;

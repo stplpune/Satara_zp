0aee5d5b4ef08e78bf415b7f3e9fc570cf59b532
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-11-08 15:50:42 +0530

    add exam-master page design and creat component

--- src/app/modules/settings/add-test/add-test.module.ts
+++ src/app/modules/settings/add-test/add-test.module.ts
@@ -3,15 +3,51 @@ import { CommonModule } from '@angular/common';
 
 import { AddTestRoutingModule } from './add-test-routing.module';
 import { AddTestComponent } from './add-test.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { TranslateModule } from '@ngx-translate/core';
+import { GlobalDialogComponent } from 'src/app/shared/components/global-dialog/global-dialog.component';
+import { PageStatisticsComponent } from 'src/app/shared/components/page-statistics/page-statistics.component';
+import { TableComponent } from 'src/app/shared/components/table/table.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { AddExamMasterComponent } from './add-exam-master/add-exam-master.component';
 
 
 @NgModule({
   declarations: [
-    AddTestComponent
+    AddTestComponent,
+    AddExamMasterComponent
   ],
   imports: [
     CommonModule,
-    AddTestRoutingModule
+    AddTestRoutingModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatInputModule, 
+    ReactiveFormsModule,
+    GlobalDialogComponent,
+    TableComponent,
+    PageStatisticsComponent,
+    TranslateModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatTableModule,
+    MatDatepickerModule
   ]
 })
 export class AddTestModule { }

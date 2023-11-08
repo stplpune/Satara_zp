161152d581600e35a432a55506a579836b10d309
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-11-08 13:21:42 +0530

    assessment-subject page design

--- src/app/modules/settings/add-subject/add-subject.module.ts
+++ src/app/modules/settings/add-subject/add-subject.module.ts
@@ -3,6 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AddSubjectRoutingModule } from './add-subject-routing.module';
 import { AddSubjectComponent } from './add-subject.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { TranslateModule } from '@ngx-translate/core';
+import { GlobalDialogComponent } from 'src/app/shared/components/global-dialog/global-dialog.component';
+import { PageStatisticsComponent } from 'src/app/shared/components/page-statistics/page-statistics.component';
+import { TableComponent } from 'src/app/shared/components/table/table.component';
+import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
@@ -11,7 +26,24 @@ import { AddSubjectComponent } from './add-subject.component';
   ],
   imports: [
     CommonModule,
-    AddSubjectRoutingModule
+    AddSubjectRoutingModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatInputModule, 
+    ReactiveFormsModule,
+    GlobalDialogComponent,
+    TableComponent,
+    PageStatisticsComponent,
+    TranslateModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatTableModule
   ]
 })
 export class AddSubjectModule { }

16439186fb5442f1db02b0f790f9c3efb50bd21b
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-11-24 17:02:31 +0530

    second dashboard page design and updated label in the json file

--- src/app/modules/dashbaord2/dashbaord2.component.ts
+++ src/app/modules/dashbaord2/dashbaord2.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashbaord2.component.scss']
 })
 export class Dashbaord2Component {
-
+  dashboardCountData = new Array();
 }
--- src/app/modules/dashbaord2/dashbaord2.module.ts
+++ src/app/modules/dashbaord2/dashbaord2.module.ts
@@ -3,6 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { Dashbaord2RoutingModule } from './dashbaord2-routing.module';
 import { Dashbaord2Component } from './dashbaord2.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { DashboardRoutingModule } from '../dashboard/dashboard-routing.module';
 
 
 @NgModule({
@@ -11,7 +25,22 @@ import { Dashbaord2Component } from './dashbaord2.component';
   ],
   imports: [
     CommonModule,
-    Dashbaord2RoutingModule
+    Dashbaord2RoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DashboardRoutingModule,
+    MatSelectModule,
+    MatCardModule,
+    NgApexchartsModule,
+    TranslateModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
 export class Dashbaord2Module { }

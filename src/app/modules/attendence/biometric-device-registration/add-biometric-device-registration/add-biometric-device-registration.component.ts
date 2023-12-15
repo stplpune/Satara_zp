80173d4c040f819919264fc8d22f91fd4456db2a
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-12-15 12:12:24 +0530

    biometric-device-registration page design with add-biometric-device-registration page design

---
+++ src/app/modules/attendence/biometric-device-registration/add-biometric-device-registration/add-biometric-device-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-biometric-device-registration',
+  templateUrl: './add-biometric-device-registration.component.html',
+  styleUrls: ['./add-biometric-device-registration.component.scss']
+})
+export class AddBiometricDeviceRegistrationComponent {
+  editFlag = true;
+}
--- src/app/modules/attendence/biometric-device-registration/biometric-device-registration.component.ts
+++ src/app/modules/attendence/biometric-device-registration/biometric-device-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AddBiometricDeviceRegistrationComponent } from './add-biometric-device-registration/add-biometric-device-registration.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-biometric-device-registration',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./biometric-device-registration.component.scss']
 })
 export class BiometricDeviceRegistrationComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'IPAddress', 'DeviceDirection'];
+  dataSource = ELEMENT_DATA;
+  constructor(public dialog: MatDialog) { }
 
+  openDialog() {
+    const dialogRef = this.dialog.open(AddBiometricDeviceRegistrationComponent, {
+      width: '600px',
+      disableClose: true,
+      autoFocus: false,
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  getTableData() {
+    throw new Error('Method not implemented.');
+  }
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  IPAddress: number;
+  DeviceDirection: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', IPAddress: 101120, DeviceDirection: 'top-right' },
+  { position: 2, name: 'Hydrogen', weight: 1.0079, symbol: 'H', IPAddress: 101120, DeviceDirection: 'top-right' },
+  { position: 3, name: 'Hydrogen', weight: 1.0079, symbol: 'H', IPAddress: 101120, DeviceDirection: 'top-right' },
+  { position: 4, name: 'Hydrogen', weight: 1.0079, symbol: 'H', IPAddress: 101120, DeviceDirection: 'top-right' },
+  { position: 5, name: 'Hydrogen', weight: 1.0079, symbol: 'H', IPAddress: 101120, DeviceDirection: 'top-right' },
+];
+

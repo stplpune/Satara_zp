f6fc67f8ad9a6e1fd37cf561acc5a347765f6a7a
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-12-22 15:40:49 +0530

    dashboard3 page design

--- src/app/modules/dashboard3/dashboard3.component.ts
+++ src/app/modules/dashboard3/dashboard3.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
-
+import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexPlotOptions, ApexStroke, ApexTooltip, ApexXAxis, ApexYAxis } from 'ng-apexcharts';
+declare var $: any;
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  stroke: ApexStroke;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions
+};
 @Component({
   selector: 'app-dashboard3',
   templateUrl: './dashboard3.component.html',
--- src/app/modules/dashboard3/dashboard3.module.ts
+++ src/app/modules/dashboard3/dashboard3.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { Dashboard3RoutingModule } from './dashboard3-routing.module';
 import { Dashboard3Component } from './dashboard3.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatSelectFilterModule } from 'mat-select-filter';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { DashboardRoutingModule } from '../dashboard/dashboard-routing.module';
 
 
 @NgModule({
@@ -11,7 +25,23 @@ import { Dashboard3Component } from './dashboard3.component';
   ],
   imports: [
     CommonModule,
-    Dashboard3RoutingModule
+    Dashboard3RoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DashboardRoutingModule,
+    MatSelectModule,
+    MatCardModule,
+    NgApexchartsModule,
+    TranslateModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectFilterModule
   ]
 })
 export class Dashboard3Module { }
